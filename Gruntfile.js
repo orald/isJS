fa229061e4ac2cd5a0d7de6fd6c3daa99d9604de
orald <orald@vbi.vt.edu>
2013-04-03 16:39:07 -0400

    minified and corrected

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
         separator: ';'
       },
       dist: {
-        src: ['lib/**/*.js'],
+        src: ['src/**/*.js'],
         dest: 'dist/<%= pkg.name %>.js'
       }
     },
